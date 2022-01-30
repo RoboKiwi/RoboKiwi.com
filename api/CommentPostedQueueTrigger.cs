438d8153bc97dfc92e7d041ddffda465db75d880
David Moore <david@robokiwi.com>
2022-01-31 01:35:28 +1300

    Disabling service bus trigger

--- api/CommentPostedQueueTrigger.cs
+++ api/CommentPostedQueueTrigger.cs
@@ -7,33 +7,33 @@ using RoboKiwi.Functions.Services;
 
 namespace RoboKiwi.Functions
 {
-    public static class CommentPostedQueueTrigger 
-    {
-        [FunctionName("CommentCheckSpam")]
-        public static void Run(
-            [ServiceBusTrigger("CommentPosted", Connection = "ServiceBus")]CommentCheckSpamMessage message,
-            [CosmosDB("robokiwi", "comments", Connection = "DocumentDatabase", Id = "{Id}", PartitionKey = "{PageId}", CreateIfNotExists = true)]out Comment comment,
-            ILogger log)
-        {
-            log.LogInformation($"Spam check for comment {message.Id} on page {message.PageId}");
+    // public static class CommentPostedQueueTrigger 
+    // {
+    //     [FunctionName("CommentCheckSpam")]
+    //     public static void Run(
+    //         [ServiceBusTrigger("CommentPosted", Connection = "ServiceBus")]CommentCheckSpamMessage message,
+    //         [CosmosDB("robokiwi", "comments", Connection = "DocumentDatabase", Id = "{Id}", PartitionKey = "{PageId}", CreateIfNotExists = true)]out Comment comment,
+    //         ILogger log)
+    //     {
+    //         log.LogInformation($"Spam check for comment {message.Id} on page {message.PageId}");
 
-            log.LogInformation("Checking comment against Akismet API");
+    //         log.LogInformation("Checking comment against Akismet API");
 
-            comment = new Comment();
-            comment.Id = message.Id;
-            comment.Pid = message.PageId;
-            comment.Content = message.Content;
-            comment.Date = message.Date;
-            comment.Email = message.Email;
-            comment.Pid = message.PageId;
-            comment.Name = message.Name;
-            comment.UserIp = message.UserIp;
-            comment.Url = message.Url;
-            comment.ReplyTo = string.IsNullOrWhiteSpace(message.ReplyTo) ? null : Guid.Parse(message.ReplyTo);
+    //         comment = new Comment();
+    //         comment.Id = message.Id;
+    //         comment.Pid = message.PageId;
+    //         comment.Content = message.Content;
+    //         comment.Date = message.Date;
+    //         comment.Email = message.Email;
+    //         comment.Pid = message.PageId;
+    //         comment.Name = message.Name;
+    //         comment.UserIp = message.UserIp;
+    //         comment.Url = message.Url;
+    //         comment.ReplyTo = string.IsNullOrWhiteSpace(message.ReplyTo) ? null : Guid.Parse(message.ReplyTo);
             
-            var result = AkismetApiClient.CommentCheck(message);
+    //         var result = AkismetApiClient.CommentCheck(message);
 
-            comment.Spam = result ? SpamClassification.ApiMarkedSpam : SpamClassification.ApiMarkedHam;
-        }
-    }
+    //         comment.Spam = result ? SpamClassification.ApiMarkedSpam : SpamClassification.ApiMarkedHam;
+    //     }
+    // }
 }
